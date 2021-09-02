ebb5069e832092b0fae8a0d737ea9552f8a5c82a
Luis Ramirez <luislibra90@hotmail.com>
2021-09-02 14:15:48 -0500

    Cascaron del proyecto

---
+++ src/main/java/com/resuelve/PruebaBackendResuelveApplication.java
@@ -0,0 +1,13 @@
+package com.resuelve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBackendResuelveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBackendResuelveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resuelve/PruebaBackendResuelveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resuelve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBackendResuelveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
