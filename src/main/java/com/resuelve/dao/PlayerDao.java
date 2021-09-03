b37b186e1a6c926797fb4857cdc2bd2bf555a957
Luis Ramirez <luislibra90@hotmail.com>
2021-09-02 20:02:16 -0500

    Implementacion de logica para calcular sueldo completo

--- src/main/java/com/resuelve/dao/PlayerDao.java
+++ src/main/java/com/resuelve/dao/PlayerDao.java
@@ -1,5 +1,7 @@
 package com.resuelve.dao;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,14 +19,47 @@ public class PlayerDao implements IPlayerDao {
 	
 	@Override
 	public Players returnPayment(Players players) {
+		Map<String, Integer> golesPorEquipo = new HashMap<>();
+		Map<String, Integer> requeridoPorEquipo = new HashMap<>();
 		players.getPlayer().forEach((p) -> {
 			Optional<ScoreByLevelEntity> optional = repository.findById(p.getNivel());
 			if (optional.isPresent()) {
 				ScoreByLevelEntity entity = optional.get();
-				System.out.println("Nivel: " + entity.getLevel() + " Goles: " + entity.getScore());
+				if(golesPorEquipo.containsKey(p.getEquipo())) {
+					int goles = golesPorEquipo.get(p.getEquipo());
+					int requerido = requeridoPorEquipo.get(p.getEquipo());
+					golesPorEquipo.put(p.getEquipo(), p.getGoles() + goles);
+					requeridoPorEquipo.put(p.getEquipo(), entity.getScore() + requerido);
+				}
+				else {
+					golesPorEquipo.put(p.getEquipo(), p.getGoles());
+					requeridoPorEquipo.put(p.getEquipo(), entity.getScore());
+				}
+			}
+		});
+		players.getPlayer().forEach((p) -> {
+			Optional<ScoreByLevelEntity> optional = repository.findById(p.getNivel());
+			if (optional.isPresent()) {
+				ScoreByLevelEntity entity = optional.get();				
+				String equipo = p.getEquipo();
+				
+				double porcEquipo = redondea((golesPorEquipo.get(equipo) * 100.0) / requeridoPorEquipo.get(equipo));
+				porcEquipo = (porcEquipo > 100) ? 100.0 : porcEquipo;
+				
+				double porcInd = redondea((p.getGoles() * 100.0) / entity.getScore());
+				porcInd = (porcInd > 100) ? 100.0 : porcInd;
+				
+				double bonoEquipo = redondea((p.getBono()/2) * porcEquipo / 100.0);
+				double bonoInd = redondea((p.getBono()/2) * porcInd / 100.0);
+				
+				p.setSueldoCompleto(p.getSueldo() + bonoEquipo + bonoInd);
 			}
 		});
 		return players;
+	}
+	
+	private double redondea(double valor) {
+		return Math.round(valor * 100.0)/100.0;
 	}
 
 }
